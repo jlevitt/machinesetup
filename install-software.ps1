f5b43d367d6ddc23d40d7d5338aee2b8503603f8
Jake Levitt <jake.levitt@rackspace.com>
2015-02-25 09:54:28 -0500

    More gems for contractacon

--- install-software.ps1
+++ install-software.ps1
@@ -5,6 +5,11 @@ choco install hxd
 choco install vim
 choco install haskellplatform
 choco install kdiff3
+
+# Used for contractacon
 choco install ruby -Version 1.9.3.55100
-choco install python2
 choco install ruby.devkit
+gem install httparty
+gem install json-schema
+gem install multi_json
+choco install python2
