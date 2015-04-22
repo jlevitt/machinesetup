4b5bcd38a56fa06dc91bc5777e7ecf6973f3b25f
Jake Levitt <jlevitt@vt.edu>
2015-04-22 16:43:44 -0400

    Add powershell profile.

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,63 @@
+Set-Location C:\projects
+
+##### Git helpers
+function Grepout($pattern)
+{
+    git checkout $(git branch | grep $pattern).Trim()
+}
+
+Set-Alias grout Grepout
+
+function DeleteMergedLocal()
+{
+    git branch --merged | grep -v "\*" | grep -v "master" | xargs -n 1 git branch -d
+}
+
+Set-Alias dml DeleteMergedLocal
+
+function ViewMergedLocal()
+{
+    git branch --merged | grep -v "\*" | grep -v "master"
+}
+
+Set-Alias vml ViewMergedLocal
+
+function KillSite($keep)
+{
+    Get-Process iisexpress |? { -Not $keep.Contains($_.Id) } | Stop-Process
+}
+
+Set-Alias ks KillSite
+
+function DeleteBinaries($path)
+{
+    if (-not $path)
+    {
+        throw "You must supply a path"
+    }
+    gci $path -Recurse -Include *.exe,*.pdb,*.dll | Remove-Item
+}
+
+Set-Alias delbin DeleteBinaries
+
+function OpenSolutions
+{
+    . $(ls *.sln)
+}
+
+Set-Alias sln OpenSolutions
+
+function TortoiseGit($command, $path)
+{
+    if (-not $path)
+    {
+        $path = "."
+    }
+    & "C:\Program Files\TortoiseGit\bin\TortoiseGitProc.exe" /command:$command /path:$path
+}
+
+Set-Alias tgit TortoiseGit
+
+# Load posh-git example profile
+. 'C:\projects\open-source\posh-git\profile.example.ps1'
+
