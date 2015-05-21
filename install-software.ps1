d47088d09c6593da4547716df68d9c4c74add8f7
Jake Levitt <jlevitt@vt.edu>
2015-05-21 13:54:40 -0400

    Add all software as of 2015-05-21 and reorganize.

--- install-software.ps1
+++ install-software.ps1
@@ -1,13 +1,28 @@
+choco install devbox-rapidee
+choco install notepadplusplus
+choco install pidgin
+choco install 7zip.install
+choco install conemu
+choco install vim
+choco install keepass
 choco install lockhunter
+choco install fiddler
 choco install NugetPackageExplorer
 choco install pandoc
 choco install hxd
-choco install vim
-choco install haskellplatform
 choco install kdiff3
+choco install haskellplatform
 choco install virtualbox
 choco install vagrant
-choco install conemu
+choco install logparser
+choco install mysql
+choco install mysql.workbench
+choco install shexview.portable
+choco install procexp
+choco install putty
+choco install pstools
+choco install windirstat
+choco install winscp
 
 # Used for contractacon
 choco install ruby -Version 1.9.3.55100
@@ -17,3 +32,35 @@ gem install json-schema
 gem install multi_json
 gem install minitest
 choco install python2
+
+# Has non choco dependencies
+choco install resharper
+
+# Other
+# * git
+# * TortoiseGit
+# * MSSoap toolkit 3.0
+# * WebServiceStudio
+# * Android Studio
+# * Box
+# * Jungle Disk Workgroup
+# * Memcache 1.2.5
+# * SQL Server
+# * SQL Server Express
+# * Data tools
+# * VS 2008,2010,2012,2013
+# * Redgate reflector
+# * Log Parser
+# * http://slntools.codeplex.com/wikipage?title=SLNTools%20Compare&referringTitle=Home
+#  * Open TortoiseGit settings
+#  * Click Diff Viewer
+#  * Click Advanced…
+#  * Click Add…
+#  * For Extension, enter .sln
+#  * For External Program enter: "C:\Program Files (x86)\Tools for SLN File\SLNTools.exe" CompareSolutions %base %mine
+# * Posh git
+
+# Settings
+# * Putty
+# * Postman
+# * Resharper
