dc50391a42a3450ba4ce31a9a07ab2afa032e8f5
Jake Levitt <jlevitt@vt.edu>
2017-06-22 22:00:35 -0400

    Update install-software.ps1

--- install-software.ps1
+++ install-software.ps1
@@ -1,3 +1,6 @@
+Set-ExecutionPolicy Unrestricted
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
 choco install devbox-rapidee -y
 choco install tortoisegit -y
 choco install git -params '"/GitAndUnixToolsOnPath /NoAutoCrlf"' -y
